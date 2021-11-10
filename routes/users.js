73019dc29d61dbbe8f1de5ec12efc4f48e7c22a1
ketank8 <ketan.kapale@live.com>
2021-11-10 18:33:08 +0530

    10th_nov

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,25 @@
 const express = require('express');
-const Userroute = express.Router();
+const route = express.Router();
 const Users = require('../models/users');
 const bcrypt =require('bcryptjs');
 
 // Update User
 
-Userroute.put('auth/users/:id' , async (req , res)=>{
-    if(req.body.userId===req.params.id){
+route.put("/:id" , async (req , res)=>{
+    if(req.body.userId === req.params.id || req.user.isAdmin){
 
         if(req.body.password){
             try{
                 const salt = await bcrypt.genSalt(10);
                 req.body.password = await bcrypt.hash(req.body.password , salt);
             }catch(err){
-                res.status(403).json(err)
+                console.log(err)
             }
         }
         try{
-            const user = await Users.findbyIdAndUpdate(req.params.id, {$set:req.body});
-            res.status(200).json('user info updated');
+            const user =  await Users.findByIdAndUpdate(req.params.id, 
+                {$set:req.body});
+            res.status(200).json({'user info updated' : user});
 
 
         }catch(err){
@@ -31,5 +32,19 @@ Userroute.put('auth/users/:id' , async (req , res)=>{
 
 })
 
-module.exports = Userroute;
+
+//delete user
+route.delete('/:id' , async (req,res)=>{
+    try{
+        if(req.body.userId ===  req.params.id){
+            await Users.deleteOne({_id: req.params.id});
+            res.status(200).json('user deleted');
+        }
+
+    }catch(err){
+        console.log(err)
+    }
+})
+
+module.exports = route;
 
